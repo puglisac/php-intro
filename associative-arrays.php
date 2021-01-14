5414cd60826fb5c8d6c1b1e8ee2df31602139041
Alan Puglisi <alan.c.puglisi@gmail.com>
2021-01-14 14:40:50 -0600

    finished assoc arrays vid

---
+++ associative-arrays.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Associative Arrays</title>
+</head>
+
+<body>
+    <!-- like a pyton dictionary or JS object -->
+    <?php $assoc = ["first_name" => "Alan", "last_name" => "Puglisi"];
+    echo "{$assoc["first_name"]} {$assoc["last_name"]}"
+    ?>
+</body>
+
+</html>
